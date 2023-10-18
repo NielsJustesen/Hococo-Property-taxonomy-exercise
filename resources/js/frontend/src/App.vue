02d16eccb1ec08dd86647ee90fd4e101e6fa3fa3
NielsJustesen <niels449@hotmail.com>
2023-10-18 20:51:29 +0200

    first setup and test

---
+++ resources/js/frontend/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>{{ test }}</div>
+</template>
+
+<script>
+import { computed } from "vue";
+import { useStore } from "vuex";
+export default {
+    name: "App",
+    setup() {
+        const store = useStore();
+        store.dispatch("fetchData");
+        const test = computed(() => {
+            return store.getters.getTest;
+        });
+        return { test };
+    },
+};
+</script>
+
+<style></style>
