f258b248fe719bedbac8be5d1234e960e504d03d
NielsJustesen <niels449@hotmail.com>
2023-10-19 12:18:27 +0200

    removed frontend, added migrations

---
+++ database/migrations/2023_10_19_084114_create_buildings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBuildingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('buildings', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->integer('height')->default(0);
+            $table->enum('type', ['Building']);
+            $table->integer('parent_id')->nullable();
+            $table->decimal('monthly_rent', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('buildings');
+    }
+}
